083ea8db29a0e58dd3ce651c119ed55b3d628d64
RamchandaniSahil <109071825+RamchandaniSahil@users.noreply.github.com>
2023-08-21 16:54:48 +0530

    Handle images with formidable

---
+++ controllers/product.controller.js
@@ -0,0 +1,86 @@
+import Product from "../models/product.schema";
+import formidable from "formidable";
+import fs from "fs";
+import { deleteFile, uploadFile } from "../services.imageUpload";
+import Mongoose from "mongoose";
+import asyncHandler from "../services/asyncHandler";
+import CustomError from "../utils/customError";
+import config from "../config/index";
+
+/***************************************************
+ * @ADD_PRODUCT
+ * @route http://localhost:4000/api/product
+ * @description Controller used for creating new product
+ * @description Only admin can create the coupon
+ * @description Uses Coludinary
+ * @return Product Object
+ ****************************************************/
+
+export const addProduct = asyncHandler(async (req, res) => {
+  const form = formidable({
+    multiples: true,
+    keepExtensions: true,
+  });
+
+  form.parse(req, async function (err, fields, files) {
+    try {
+      if (err) {
+        throw new CustomError(err.message || "Something went wrong", 500);
+      }
+      // const productId = new Mongoose.Types.ObjectId().toHexString();
+      //   console.log(fields, files);
+
+      // Check for fields
+      if (
+        !fields.name ||
+        !fields.price ||
+        !fields.description ||
+        !fields.collectionId
+      ) {
+        throw new CustomError("Please fill all the details", 500);
+      }
+
+      // handling images
+      let imgArrayRes = Promise.all(
+        Object.keys(files).map(async (fileKey, index) => {
+          const element = files[fileKey];
+
+          const data = fs.readFileSync(element.filePath);
+
+          const upload = await uploadFile({
+            // bucketName: config.BUCKET_NAME,
+            // key: `products/${productId}/photo_${index + 1}.png`,
+            body: data,
+            // contentType: element.mimetype,
+          });
+          return {
+            secure_url: upload.Location,
+          };
+        })
+      );
+
+      let imgArray = await imgArrayRes;
+
+      const product = await Product.create({
+        // _id: productId,
+        photos: imgArray,
+        ...fields,
+      });
+
+      if (!product) {
+        throw new CustomError("Product was not created", 400);
+      }
+      res.status(200).json({
+        success: true,
+        product,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message || "Something went weont",
+      });
+    }
+  });
+});
+
+// 51.00
